7c5c78ff98e5b7e68bf2e7c88755c404606f07ab
aryansingh120 <156301216+aryansingh120@users.noreply.github.com>
2024-08-20 14:32:50 +0530

    Add files via upload

---
+++ 2.conditional statement/if else if/1_check_number_positive_or_negative_or_zero.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("%d is positive",a);
+    }
+    else if(a<0)
+    {
+        printf("%d is negative",a);
+    }
+    else
+    {
+        printf("%d is Zero",a);
+    }
+    return 0;
+}
